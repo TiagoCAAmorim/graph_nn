864784bd869dff2a482d728d57260ebb26ffb1fc
Tiago Amorim <tiago.ca.amorim@gmail.com>
2025-02-09 19:18:43 -0300

    Testing EncDecNetwork

--- gnn_lineq/runs/run_graph_conv.py
+++ gnn_lineq/runs/run_graph_conv.py
@@ -9,16 +9,16 @@ import torch
 from torch_geometric.loader import DataLoader
 import matplotlib.pyplot as plt
 
-from networks import GraphConvNetwork
+from networks import TransformerConvLayer, EncDecNetwork
 from samples import LinEqSample, DynamicGraphDataset
-from utils import count_parameters, plot_samples_error, plot_dataset_error, train_model, plot_losses
+from utils import plot_samples_error, plot_dataset_error, train_model, plot_losses
 
 
 def build_dataset():
     """Define dataset."""
     options = {
-        'rank': 5,
-        'diagonals': 4,
+        'rank': 50,
+        'diagonals': 10,
         'off_diagonal_abs_mean': 0.5,
         'symmetric': False,
         'width_range': (0.1,10.)
@@ -42,24 +42,28 @@ def main():
     print(f'Using device: {device}')
 
     dataset = build_dataset()
-    loader = DataLoader(dataset, batch_size=32, shuffle=False)
+    loader = DataLoader(dataset, batch_size=256, shuffle=False)
 
-    model = GraphConvNetwork(
-            input_dim=dataset.num_node_features,
-            output_dim=dataset.num_features,
-            hidden_dim=16,
-            layers=5,
-            p_drop=0.2,
-            activation='LeakyReLU',
-            negative_slope=0.2,
+    model = EncDecNetwork(
+        input_dims=(dataset.num_node_features, dataset.num_edge_features),
+        lat_dims=(8, 4),
+        output_dim=1,
+        process_block=TransformerConvLayer,
+        n_process_blocks=2,
+        add_skip=True,
+        mlp_layers=3,
+        p_drop=0.00,
+        add_layer_norm=False,
+        activation='relu',
+        # negative_slope=0.1,
+        heads=3,
+        beta=False,
     )
-    print(f'Number of parameters: {count_parameters(model):,}')
-    print(model)
 
     print('\nTraining the model...')
     model = model.to(device)
-    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.01, weight_decay=5e-4)
-    loss = train_model(model, loader, epochs=20, print_epochs=10, optimizer=optimizer)
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.001, weight_decay=5e-4)
+    loss = train_model(model, loader, epochs=200, print_epochs=20, optimizer=optimizer)
 
     print('Plotting the results...')
     folder = Path(__file__).resolve().parent / '_plots'
